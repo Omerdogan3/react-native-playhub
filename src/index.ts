38d72c792679467b40419881b351ddd1d28026ba
Omer Dogan <omerdogan3@gmail.com>
2021-12-15 18:48:12 +0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,6 @@
-import dynamicLinks from '@react-native-firebase/dynamic-links';
-import {setData, getData} from '../helpers';
-
-import {readChapterEvent, downloadEvent} from '../api';
-
-export function initialize(){
-  syncUid()
-  // getData("uid").then((uid)=> {
-  //   console.log("uid", uid)
-  // })
-  readChapterEvent(10)
-}
-
-const syncUid = async () => {
-  try {
-    const link = await dynamicLinks().getInitialLink()    
-    if(link && link.url){
-      const uid = link.url.split("uid=")[1];
-      const exUid = await getData("uid");
-      setData("uid", uid)
-
-      if(!exUid){
-        // download event
-        downloadEvent()
-      }
-
-    }
-  } catch (e) {
-    // saving error
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
   }
-}
-
-
-export const readChapter = (eventValue: number) => {
-  readChapterEvent(eventValue)
-}
+  return a + b;
+};
