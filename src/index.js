deabcc3e05fa4b77c56d4514200c7679305f9561
Omer Dogan <omerdogan3@gmail.com>
2021-12-27 16:02:17 +0300

    crypto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import {Alert} from 'react-native';
 import dynamicLinks from '@react-native-firebase/dynamic-links';
 import {setData, getData} from './helpers';
 import {readChapterEvent, highScoreEvent, downloadEvent} from './api';
+import CryptoJS from 'crypto-js';
 
 export function initialize(){
   syncUid()
@@ -9,16 +11,20 @@ export function initialize(){
 const syncUid = async () => {
   try {
     const link = await dynamicLinks().getInitialLink()    
+    
     if(link && link.url){
-      const uid = link.url.split("uid=")[1];
+      let uid = link.url.split("uid=")[1];
+      uid = CryptoJS.AES.decrypt(uid, "361.Rocks").toString(CryptoJS.enc.Utf8);
+
       const exUid = await getData("uid");
+
+      // Alert.alert(JSON.stringify(uid))
+
       setData("uid", uid)
 
       if(!exUid){
-        // download event
         downloadEvent()
       }
-
     }
   } catch (e) {
     // saving error
